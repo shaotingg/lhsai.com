b9f45554a8e774af5e1502a2369df4d54970bd04
shaoting <shaoting@fantem.com>
2018-11-19 10:39:20 +0800

    update the framework and add vuetify css framework

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,39 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list v-for="(item, index) in links" :key="index">
+        <v-list-tile :to="item.link">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <nuxt/>
+    </v-content>
+  </v-app>
 </template>
-
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          title: '首页',
+          link: '/'
+        },
+        {
+          title: 'css样式',
+          link: '/'
+        },
+        {
+          title: '前端框架',
+          link: '/'
+        }
+      ]
+    }
+  }
 }
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
-
+</script>
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,32 @@
-module.exports = {
+export default {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'bob',
+    title: 'lhsai.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'nuxt templete' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ],
+    script: [
+
     ]
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  modules: [
+    '@nuxtjs/vuetify'
+  ],
   /*
   ** Build configuration
   */
@@ -24,14 +34,10 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
+   extractCSS: true,
     extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+      if (isDev) {
+        config.devtool = 'eval-source-map'
       }
     }
   }
